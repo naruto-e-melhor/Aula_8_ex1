35874619c15c8b4d3ac46bc89c9c4bca6378e095
naruto-e-melhor <102871291+naruto-e-melhor@users.noreply.github.com>
2022-06-04 09:40:52 -0300

    Add files via upload

--- java.js
+++ java.js
@@ -2,22 +2,15 @@ let num1 = parseInt(prompt("Digite um número:"));
 let num2 = parseInt(prompt("Digite outro número:"));
 let result;
 let other;
-let equi;
 
 if (num1 > num2){
     result = num1;
     other = num2;
-    equi = 0;
-}if (num1 < num2){
+    alert(`O maior número é ${result}, e o menor é ${other}`)
+} else if (num1 < num2){
     result = num2;
     other = num1;
-    equi = 0;
-}if (num1 == num2) {
-    equi = 1;
-}
-if(equi = 0){
-alert(`O maior número é ${result}, e o menor é ${other}`)
-}
-if(equi = 1){
+    alert(`O maior número é ${result}, e o menor é ${other}`)
+}else if (num1 == num2) {
     alert(`Os dois números são iguais`)
 }
